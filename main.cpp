ff72d6a591e09b5712673d5afef9d6a8a40f05b2
Aadit Kolar <aadit.kolar@gmail.com>
2020-06-02 00:38:37 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "XPLMDisplay.h"
+#include "XPLMGraphics.h"
+#include <string.h>
+#if IBM
+	#include <windows.h>
+#endif
+#if LIN
+	#include <GL/gl.h>
+#elif __GNUC__
+	#include <OpenGL/gl.h>
+#else
+	#include <GL/gl.h>
+#endif
+
+PLUGIN_API int XPluginStart(char * outName, char * outSig,	char * outDesc){
+	strcpy(outName, "XSharedCockpit");
+	strcpy(outSig, "com.thecodingaviator.xsharedcockpit");
+	strcpy(outDesc, "A plugin to allow you to fly together with your friends");
+
+	return 1;
+}
+
+PLUGIN_API void	XPluginStop(void){
+	//Literally nothing because the plugin doesn't do anything just yet
+}
+
+PLUGIN_API void XPluginDisable(void) { }
+PLUGIN_API int  XPluginEnable(void)  { return 1; }
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { }
