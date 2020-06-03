6e054529184f3cf6f388064b4f49d7b185f83ea2
Aadit Kolar <aadit.kolar@gmail.com>
2020-06-04 01:31:47 +0530

    did a whoooole bunch of stuff. broadcaster and receiver works, but don't take array datarefs into account. going to fix tomorrow

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 //External libraries
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
 #include "XPLMMenus.h"
 #include "XPLMProcessing.h"
 #include "XPLMDataAccess.h"
@@ -13,18 +15,19 @@
 #include <sstream>
 #include <vector>
 #include <iterator>
-#include <evpp/udp/udp_server.h>
-#include <evpp/udp/udp_message.h>
 
-#if IBM
-	#include <windows.h>
-#endif
+//#if IBM
+//	#include <windows.h>
+//#endif
 
 //Using object defs
 using std::vector;
 using std::string;
 using std::size_t;
 using std::istringstream;
+using boost::asio::ip::udp;
+using boost::asio::ip::address;
+
 
 //Using function defs
 using std::to_string;
@@ -36,17 +39,26 @@ using std::stof;
 using std::back_inserter;
 using std::getline;
 
+//Namespace using defs
+using namespace boost::placeholders;
+
 //Variable defs
 int menu_container_index;
 XPLMMenuID menu_id;
 vector<string> dref_strings;
 vector <XPLMDataRef> drefs;
 vector <XPLMDataTypeID> dref_types;
+boost::asio::io_service io_service;
+udp::socket master{ io_service };
+udp::endpoint master_endpoint{};
+udp::socket slave{ io_service };
+udp::endpoint slave_endpoint;
+
 
 //Constant defs
 const string master_address = "127.0.0.1";
 const int master_port = 49000;
-const string slave_adrress = "127.0.0.1";
+const string slave_address = "127.0.0.1";
 const int slave_port = 49001;
 bool running = false;
 bool is_master = false;
@@ -57,6 +69,8 @@ void menu_handler(void*, void*);
 void load_plugin();
 float loop(float elapsed1, float elapsed2, int ctr, void* refcon);
 bool number_contains(int number, int check);
+void toggle_master();
+void toggle_slave();
 
 
 //X-Plane API functions
@@ -65,13 +79,13 @@ PLUGIN_API int XPluginStart(char * outName, char * outSig,	char * outDesc){
 	strcpy(outSig, "com.thecodingaviator.xsharedcockpit");
 	strcpy(outDesc, "A plugin to allow you to fly together with your friends");
 
+	XPLMDebugString("XSharedCockpit has started init\n");
+
 	menu_container_index = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "XSharedCockpit", 0, 0);
 	menu_id = XPLMCreateMenu("Sample Menu", XPLMFindPluginsMenu(), menu_container_index, menu_handler, NULL);
 	XPLMAppendMenuItem(menu_id, "Toggle Master", (void*)"Toggle Master", 1);
 	XPLMAppendMenuItem(menu_id, "Toggle Slave", (void*)"Toggle Slave", 1);
-	XPLMAppendMenuItem(menu_id, "Reload all plugins", (void*)"Reload Plugins", 1);
 
-	load_plugin();
 
 	dref_strings.push_back("sim/flightmodel/position/local_x");
 	dref_strings.push_back("sim/flightmodel/position/local_y");
@@ -86,7 +100,7 @@ PLUGIN_API int XPluginStart(char * outName, char * outSig,	char * outDesc){
 	dref_strings.push_back("sim/flightmodel/position/local_vy");
 	dref_strings.push_back("sim/flightmodel/position/local_vz");
 
-	XPLMDebugString("XSharedCockpit has started");
+	XPLMDebugString("XSharedCockpit has started\n");
 	return 1;
 }
 
@@ -99,18 +113,19 @@ PLUGIN_API void XPluginDisable(void) {
 }
 
 PLUGIN_API int XPluginEnable(void) {
-	XPLMDebugString("XSharedCockpit initializing");
+	XPLMDebugString("XSharedCockpit initializing\n");
+	//load_plugin();
 	for (auto const& dref_name : dref_strings) {
 		auto dref = XPLMFindDataRef(dref_name.c_str());
 		drefs.push_back(dref);
 		dref_types.push_back(XPLMGetDataRefTypes(dref));
 	}
 
-	XPLMDebugString("XSharedCockpit initialized");
+	XPLMDebugString("XSharedCockpit initialized\n");
 	return 1; 
 }
 
-PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { }
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) {}
 
 void load_plugin() {
 	char name[512];
@@ -120,7 +135,7 @@ void load_plugin() {
 	size_t pos = cfg_file_path.find(name);
 	cfg_file_path = cfg_file_path.substr(0, pos);
 	cfg_file_path.append("smartcopilot.cfg");
-	XPLMDebugString(cfg_file_path.c_str());
+	//XPLMDebugString(cfg_file_path.c_str());
 	CSimpleIniA ini;
 	ini.SetUnicode();
 	ini.LoadFile(cfg_file_path.c_str());
@@ -132,6 +147,7 @@ void menu_handler(void* in_menu_ref, void* in_item_ref) {
 		toggle_master();
 	}
 	else if (!strcmp((const char*)in_item_ref, "Toggle Slave"))	{
+		XPLMDebugString("Toggling slave\n");
 		toggle_slave();
 	}
 }
@@ -154,12 +170,12 @@ void send_datarefs() {
 	string dref_string = "";
 
 	for (int i = 0; i < drefs.size(); i++) {
-		auto type = dref_types.at(i);
+		XPLMDataTypeID type = dref_types.at(i);
 		string value;
 		auto dref = drefs.at(i);
 		if (type == xplmType_Int) {
 			value = to_string(XPLMGetDatai(dref));
-		} else if (type == xplmType_Float) {
+		} else if (type == xplmType_Float || type == (xplmType_Float | xplmType_Double)) {
 			value = to_string(XPLMGetDataf(dref));
 		} else if (type == xplmType_Double) {
 			value = to_string(XPLMGetDatad(dref));
@@ -174,22 +190,21 @@ void send_datarefs() {
 		}
 	}
 
-	XPLMDebugString(dref_string.c_str());
+	boost::system::error_code err;
+	master.send_to(boost::asio::buffer(dref_string), slave_endpoint, 0, err);
 }
 
-template <typename Out>
-void split(const std::string& s, char delim, Out result) {
-	istringstream iss(s);
-	string item;
-	while (getline(iss, item, delim)) {
-		*result++ = item;
+vector<string> split(std::string s, const char delimiter) {
+	vector<string> output;
+	auto delimiter_pos = s.find(delimiter);
+	if (delimiter_pos > s.size()) {
+		output.push_back(s);
 	}
-}
-
-vector<string> split(const std::string& s, char delim) {
-	vector<string> elems;
-	split(s, delim, back_inserter(elems));
-	return elems;
+	else {
+		output.push_back(s.substr(0, delimiter_pos));
+		output.push_back(s.substr(delimiter_pos + 1, s.size() - 1));
+	}
+	return output;
 }
 
 void sync_datarefs() {
@@ -197,9 +212,21 @@ void sync_datarefs() {
 		return;
 	}
 
-	string recieved; // This is a dummy string right now, I will implement UDP soon
-	
-	vector<string> dref_value_strings = split(recieved, ' ');
+	const auto available = slave.available();
+
+	if (available == 0) {
+		return;
+	}
+
+	char* buffer = new char[available];
+	auto bytes_transferred = slave.receive_from(boost::asio::buffer(buffer, available), master_endpoint);
+
+	string received(buffer);
+	received = received.substr(0, bytes_transferred);
+	XPLMDebugString(received.c_str());
+	XPLMDebugString("\n");
+
+	vector<string> dref_value_strings = split(received, ' ');
 
 	for (int i = 0; i < dref_value_strings.size(); i++) {
 		auto dref_value_string = dref_value_strings.at(i);
@@ -210,10 +237,12 @@ void sync_datarefs() {
 		if (type == xplmType_Int) {
 			auto value = stoi(values[1]);
 			XPLMSetDatai(dref, value);
-		} else if (type == xplmType_Float) {
+		}
+		else if (type == xplmType_Float || type == (xplmType_Float | xplmType_Double)) {
 			auto value = stof(values[1]);
 			XPLMSetDataf(dref, value);
-		} else if (type == xplmType_Double) {
+		}
+		else if (type == xplmType_Double) {
 			auto value = stod(values[1]);
 			XPLMSetDatad(dref, value);
 		}
@@ -221,11 +250,27 @@ void sync_datarefs() {
 }
 
 void start_master() {
-
+	is_master = true;
+	running = true;
+	XPLMDebugString("Declaring socket and endpoints\n");
+	master = udp::socket(io_service);
+	master_endpoint = udp::endpoint(address::from_string(master_address), master_port);
+	slave_endpoint = udp::endpoint(address::from_string(slave_address), slave_port);
+	XPLMDebugString("Opening and binding socket\n");
+	master.open(udp::v4());
+	master.bind(master_endpoint);
+	is_connected = true;
+	XPLMRegisterFlightLoopCallback(loop, 2, NULL);
 }
 
 void stop_master() {
-
+	XPLMUnregisterFlightLoopCallback(loop , NULL);
+	string close("close");
+	boost::system::error_code err;
+	master.send_to(boost::asio::buffer(close), slave_endpoint, 0, err);
+	master.close();
+	is_connected = false;
+	running = false;
 }
 
 void toggle_master() {
@@ -237,15 +282,38 @@ void toggle_master() {
 }
 
 void start_slave() {
-
+	auto type = XPLMGetDataRefTypes(XPLMFindDataRef("sim/operation/override/override_planepath[0]"));
+	XPLMDebugString(to_string(type).c_str());
+	XPLMDebugString("\n");
+	is_master = false;
+	running = true;
+	XPLMDebugString("Starting slave\n");
+	slave = udp::socket(io_service);
+	XPLMDebugString("Declaring endpoints\n");
+	master_endpoint = udp::endpoint(address::from_string(master_address), master_port);
+	slave_endpoint = udp::endpoint(address::from_string(slave_address), slave_port);
+	XPLMDebugString("Opening socket\n");
+	slave.open(udp::v4());
+	XPLMDebugString("Binding socket\n");
+	slave.bind(slave_endpoint);
+	XPLMDebugString("Setting flight path override\n");
+	XPLMSetDatai(XPLMFindDataRef("sim/operation/override/override_planepath[0]"), 1);
+	is_connected = true;
+	XPLMRegisterFlightLoopCallback(loop, 2, NULL);
 }
 
 void stop_slave() {
-
+	XPLMUnregisterFlightLoopCallback(loop, NULL);
+	slave.close();
+	XPLMDebugString("Disabling flight path override\n");
+	XPLMSetDatai(XPLMFindDataRef("sim/operation/override/override_planepath[0]"), 0);
+	is_connected = false;
+	running = false;
 }
 
 void toggle_slave() {
 	if (!running) {
+		XPLMDebugString("Running function start slave\n");
 		start_slave();
 	} else if (running && !is_master) {
 		stop_slave();
