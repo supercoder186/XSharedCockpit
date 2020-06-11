9cc9f40e8c142914fa2bd53009ad843467f9f245
Aadit Kolar <aadit.kolar@gmail.com>
2020-06-11 16:48:37 +0530

    Made data transfer two-way - slave can now broadcast commands and trigger drefs!

--- main.cpp
+++ main.cpp
@@ -47,10 +47,16 @@ using namespace boost::placeholders;
 //Variable defs
 int menu_container_index;
 XPLMMenuID menu_id;
-vector<string> dref_strings;
-vector <XPLMDataRef> drefs;
-vector <XPLMDataTypeID> dref_types;
-vector <int> indexes;
+//Sent by the master
+vector<string> master_dref_strings;
+vector <XPLMDataRef> master_drefs;
+vector <XPLMDataTypeID> master_dref_types;
+vector <int> master_indexes;
+//Sent by the slave
+vector<string> slave_dref_strings;
+vector <XPLMDataRef> slave_drefs;
+vector <XPLMDataTypeID> slave_dref_types;
+vector <int> slave_indexes;
 vector<string> master_overrides;
 vector<string> slave_overrides;
 vector<string> command_strings;
@@ -78,7 +84,9 @@ void load_plugin();
 float loop(float elapsed1, float elapsed2, int ctr, void* refcon);
 bool number_contains(int number, int check);
 int toggle_master(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
+void stop_master();
 int toggle_slave(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
+void stop_slave();
 int command_handler(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
 vector<string> split_once(string s, const char delimiter);
 
@@ -100,18 +108,18 @@ PLUGIN_API int XPluginStart(char * outName, char * outSig,	char * outDesc){
 	XPLMAppendMenuItemWithCommand(menu_id, "Toggle Master", command);
 	XPLMAppendMenuItemWithCommand(menu_id, "Toggle Slave", command2);
 
-	dref_strings.push_back("sim/flightmodel/position/local_x");
-	dref_strings.push_back("sim/flightmodel/position/local_y");
-	dref_strings.push_back("sim/flightmodel/position/local_z");
-	dref_strings.push_back("sim/flightmodel/position/psi");
-	dref_strings.push_back("sim/flightmodel/position/theta");
-	dref_strings.push_back("sim/flightmodel/position/phi");
-	dref_strings.push_back("sim/flightmodel/position/P");
-	dref_strings.push_back("sim/flightmodel/position/Q");
-	dref_strings.push_back("sim/flightmodel/position/R");
-	dref_strings.push_back("sim/flightmodel/position/local_vx");
-	dref_strings.push_back("sim/flightmodel/position/local_vy");
-	dref_strings.push_back("sim/flightmodel/position/local_vz");
+	master_dref_strings.push_back("sim/flightmodel/position/local_x");
+	master_dref_strings.push_back("sim/flightmodel/position/local_y");
+	master_dref_strings.push_back("sim/flightmodel/position/local_z");
+	master_dref_strings.push_back("sim/flightmodel/position/psi");
+	master_dref_strings.push_back("sim/flightmodel/position/theta");
+	master_dref_strings.push_back("sim/flightmodel/position/phi");
+	master_dref_strings.push_back("sim/flightmodel/position/P");
+	master_dref_strings.push_back("sim/flightmodel/position/Q");
+	master_dref_strings.push_back("sim/flightmodel/position/R");
+	master_dref_strings.push_back("sim/flightmodel/position/local_vx");
+	master_dref_strings.push_back("sim/flightmodel/position/local_vy");
+	master_dref_strings.push_back("sim/flightmodel/position/local_vz");
 
 	XPLMDebugString("XSharedCockpit has started\n");
 	return 1;
@@ -122,20 +130,27 @@ PLUGIN_API void	XPluginStop(void) {
 }
 
 PLUGIN_API void XPluginDisable(void) {
-	drefs.clear();
+	if (running) {
+		if (is_master) {
+			stop_master();
+		}
+		else {
+			stop_slave();
+		}
+	}
 }
 
 PLUGIN_API int XPluginEnable(void) {
 	XPLMDebugString("XSharedCockpit initializing\n");
-	for (auto const& dref_name : dref_strings) {
+	for (auto const& dref_name : master_dref_strings) {
 		auto values = split_once(dref_name, '[');
 
 		auto name = values[0];
 		auto dref = XPLMFindDataRef(name.c_str());
-		drefs.push_back(dref);
+		master_drefs.push_back(dref);
 
 		auto type = XPLMGetDataRefTypes(dref);
-		dref_types.push_back(XPLMGetDataRefTypes(dref));
+		master_dref_types.push_back(XPLMGetDataRefTypes(dref));
 	}
 	
 
@@ -175,29 +190,29 @@ void load_plugin() {
 	string line;
 	string current_section;
 
-	dref_strings.clear();
-	drefs.clear();
-	dref_types.clear();
+	master_dref_strings.clear();
+	master_drefs.clear();
+	master_dref_types.clear();
 	commands.clear();
 	command_strings.clear();
 	command_pointers.clear();
-	indexes.clear();
+	master_indexes.clear();
 	master_overrides.clear();
 	slave_overrides.clear();
 	command_string = "";
 
-	dref_strings.push_back("sim/flightmodel/position/local_x");
-	dref_strings.push_back("sim/flightmodel/position/local_y");
-	dref_strings.push_back("sim/flightmodel/position/local_z");
-	dref_strings.push_back("sim/flightmodel/position/psi");
-	dref_strings.push_back("sim/flightmodel/position/theta");
-	dref_strings.push_back("sim/flightmodel/position/phi");
-	dref_strings.push_back("sim/flightmodel/position/P");
-	dref_strings.push_back("sim/flightmodel/position/Q");
-	dref_strings.push_back("sim/flightmodel/position/R");
-	dref_strings.push_back("sim/flightmodel/position/local_vx");
-	dref_strings.push_back("sim/flightmodel/position/local_vy");
-	dref_strings.push_back("sim/flightmodel/position/local_vz");
+	master_dref_strings.push_back("sim/flightmodel/position/local_x");
+	master_dref_strings.push_back("sim/flightmodel/position/local_y");
+	master_dref_strings.push_back("sim/flightmodel/position/local_z");
+	master_dref_strings.push_back("sim/flightmodel/position/psi");
+	master_dref_strings.push_back("sim/flightmodel/position/theta");
+	master_dref_strings.push_back("sim/flightmodel/position/phi");
+	master_dref_strings.push_back("sim/flightmodel/position/P");
+	master_dref_strings.push_back("sim/flightmodel/position/Q");
+	master_dref_strings.push_back("sim/flightmodel/position/R");
+	master_dref_strings.push_back("sim/flightmodel/position/local_vx");
+	master_dref_strings.push_back("sim/flightmodel/position/local_vy");
+	master_dref_strings.push_back("sim/flightmodel/position/local_vz");
 
 	while (fin) {
 		getline(fin, line);
@@ -225,7 +240,7 @@ void load_plugin() {
 				}
 			}
 			else if (current_section == "CLICKS") {
-				dref_strings.push_back(match[1].str().append("_SCP"));
+				master_dref_strings.push_back(match[1].str().append("_SCP"));
 			}
 			else if (current_section == "COMMANDS") {
 				command_strings.push_back(match[1]);
@@ -253,14 +268,18 @@ void load_plugin() {
 					slave_port = stoi(match[2]);
 				}
 			}
-			else if(current_section != "WEATHER" && current_section != "SLOW"){
-				dref_strings.push_back(match[1]);
+			else if (current_section == "TRIGGERS" || current_section == "TRANSPONDER") {
+				master_dref_strings.push_back(match[1]);
+				slave_dref_strings.push_back(match[1]);
+			}
+			else if(current_section == "CONTINUED" || current_section == "SEND_BACK"){
+				master_dref_strings.push_back(match[1]);
 			}
 		}
 	}
 	fin.close();
 
-	for (auto dref_name : dref_strings) {
+	for (auto dref_name : master_dref_strings) {
 		string name;
 		vector<string> values;
 		if (!string_contains(name, "_FIXED_INDEX_")) {
@@ -274,22 +293,54 @@ void load_plugin() {
 		}
 
 		auto dref = XPLMFindDataRef(name.c_str());
-		drefs.push_back(dref);
+		master_drefs.push_back(dref);
 
 		auto type = XPLMGetDataRefTypes(dref);
 
 		if (type == xplmType_FloatArray || type == xplmType_IntArray) {
 			if (values.size() > 1) {
-				indexes.push_back(stoi(values[1]));
+				master_indexes.push_back(stoi(values[1]));
 			}
 			else {
-				indexes.push_back(0);
+				master_indexes.push_back(0);
 			}
 		}
 		else {
-			indexes.push_back(-1);
+			master_indexes.push_back(-1);
 		}
-		dref_types.push_back(type);
+		master_dref_types.push_back(type);
+	}
+
+	for (auto dref_name : slave_dref_strings) {
+		string name;
+		vector<string> values;
+		if (!string_contains(name, "_FIXED_INDEX_")) {
+			values = split_once(dref_name, '[');
+			name = values[0];
+		}
+		else {
+			name = dref_name;
+			replace(name, "_FIXED_INDEX_", "");
+			values.push_back(name);
+		}
+
+		auto dref = XPLMFindDataRef(name.c_str());
+		slave_drefs.push_back(dref);
+
+		auto type = XPLMGetDataRefTypes(dref);
+
+		if (type == xplmType_FloatArray || type == xplmType_IntArray) {
+			if (values.size() > 1) {
+				slave_indexes.push_back(stoi(values[1]));
+			}
+			else {
+				slave_indexes.push_back(0);
+			}
+		}
+		else {
+			slave_indexes.push_back(-1);
+		}
+		slave_dref_types.push_back(type);
 	}
 
 	int idx = 0;
@@ -304,11 +355,9 @@ void load_plugin() {
 		}
 		idx++;
 	}
-
-
 }
 
-//Menu handler
+//Dummy menu handler
 void menu_handler(void* in_menu_ref, void* in_item_ref) {
 
 }
@@ -364,12 +413,14 @@ void set_dataref(XPLMDataRef dref, string value_string, XPLMDataTypeID type, int
 		XPLMSetDatad(dref, value);
 	}
 	else if (type == xplmType_IntArray) {
-		int index = indexes.at(i);
+		int index = (!is_master) ? master_indexes.at(i) : slave_indexes.at(i);
+
 		int value[1] = { stoi(value_string) };
 		XPLMSetDatavi(dref, value, index, 1);
 	}
 	else if (type == xplmType_FloatArray) {
-		int index = indexes.at(i);
+		int index = (!is_master) ? master_indexes.at(i) : slave_indexes.at(i);
+
 		float value[1] = { stof(value_string) };
 		XPLMSetDatavf(dref, value, index, 1);
 	}	
@@ -387,13 +438,15 @@ string get_dataref(XPLMDataRef dref, XPLMDataTypeID type, int i) {
 		value = to_string(XPLMGetDatad(dref));
 	}
 	else if (type == xplmType_IntArray) {
-		int index = indexes.at(i);
+		int index = (is_master) ? master_indexes.at(i) : slave_indexes.at(i);
+		
 		int value_arr[1];
 		XPLMGetDatavi(dref, value_arr, index, 1);
 		value = to_string(value_arr[0]);
 	}
 	else if (type == xplmType_FloatArray) {
-		int index = indexes.at(i);
+		int index = (is_master) ? master_indexes.at(i) : slave_indexes.at(i);
+
 		float value_arr[1];
 		XPLMGetDatavf(dref, value_arr, index, 1);
 		value = to_string(value_arr[0]);
@@ -409,27 +462,30 @@ void send_datarefs() {
 
 	string dref_string = "";
 
+	vector<XPLMDataRef> drefs = (is_master) ? master_drefs : slave_drefs;
+	vector<XPLMDataTypeID> dref_types = (is_master) ? master_dref_types : slave_dref_types;
+
 	for (int i = 0; i < drefs.size(); i++) {
 		XPLMDataTypeID type = dref_types.at(i);
 		auto dref = drefs.at(i);
 
 		string value = get_dataref(dref, type, i);
 
-		if (!value.empty())
-			dref_string = dref_string
+		dref_string = dref_string
 			.append(value)
 			.append(" ");
-		else {
-			XPLMDebugString(string("No value found for ").append(dref_strings.at(i)).append("\n").c_str());
-			dref_string = dref_string.append(" ");
-		}
 	}
 
 	dref_string = dref_string.append(command_string);
 	command_string = ",";
 
 	boost::system::error_code err;
-	master.send_to(boost::asio::buffer(dref_string), slave_endpoint, 0, err);
+	if (is_master) {
+		master.send_to(boost::asio::buffer(dref_string), slave_endpoint, 0, err);
+	}
+	else {
+		slave.send_to(boost::asio::buffer(dref_string), master_endpoint, 0, err);
+	}
 }
 
 void sync_datarefs() {
@@ -437,19 +493,22 @@ void sync_datarefs() {
 		return;
 	}
 
-	const auto available = slave.available();
+	udp::endpoint receive_endpoint = (is_master) ? slave_endpoint : master_endpoint;
+	const size_t available = (is_master) ? master.available() : slave.available();
 
 	if (available == 0) {
 		return;
 	}
 
 	char* buffer = new char[available];
-	auto bytes_transferred = slave.receive_from(boost::asio::buffer(buffer, available), master_endpoint);
+	size_t bytes_transferred = (is_master) ? master.receive_from(boost::asio::buffer(buffer, available), receive_endpoint) : slave.receive_from(boost::asio::buffer(buffer, available), receive_endpoint);
 
 	string received(buffer);
 	received = received.substr(0, bytes_transferred);
 
 	vector<string> dref_value_strings = split(received, ' ');
+	vector<XPLMDataTypeID> dref_types = (is_master) ? slave_dref_types : master_dref_types;
+	vector<XPLMDataRef> drefs = (is_master) ? slave_drefs : master_drefs;
 
 	for (int i = 0; i < dref_value_strings.size() - 1; i++) {
 		auto dref_value_string = dref_value_strings.at(i);
@@ -598,12 +657,8 @@ int command_handler(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* in
 //Loop callback
 float loop(float elapsed1, float elapsed2, int ctr, void* refcon) {
 	if (running) {
-		if (is_master) {
-			send_datarefs();
-		}
-		else {
-			sync_datarefs();
-		}
+		send_datarefs();
+		sync_datarefs();
 	}
 	return -1;
 }
